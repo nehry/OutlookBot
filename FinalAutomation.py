0a48fae1a9eeeed8f1d7ad6ebd6be0c2fa033f47
Henry <62581075+nehry@users.noreply.github.com>
2021-12-20 12:47:21 +1100

    Changed flow of automation.
    Changes include:
    - Utilising matrixes to bulk-update spreadsheets all at once.
    - Changed the way the bot detects empty rows in the spreadsheet.
    - Cleared useless comments

--- FinalAutomation.py
+++ FinalAutomation.py
@@ -4,148 +4,95 @@ import sys
 import time
 import xlsxwriter
 
-#Setting up the connection to Outlook
+#Google Sheet Automation:tting up the connection to Outlook
 outlook = client.Dispatch('Outlook.Application')
 namespace = outlook.GetNameSpace('MAPI')
 account = namespace.Folders['macquarie.instructions@legalstream.com.au']
 inbox = account.Folders['Inbox']
 
-#OLD GLOBAL VARIABLES, IF CODE DOESN'T WORK, USE THIS:
-# messages = inbox.Items
-# message = messages.GetLast()
-
 #Empty List to start storing subject line and time and date
-emptyList = []
-timeList = []
+emptylist2 = []
+emptylist3 = []
+dataList2 = []
 
-# #Excel Automation for testing purposes on an excel sheet before implementing it on a google sheet.
-# workbook = xlsxwriter.Workbook(r'D:\Projects for SQL & Tableau\Python Project\instructions.xlsx')
-# worksheet = workbook.add_worksheet()
-# worksheet.write(0, 0, "Date and Time")
-# worksheet.write(0, 1, "APP Number")
-
-#Google Sheet Automation:
 scope = ['https://www.googleapis.com/auth/spreadsheets,' 'https://www.googleapis.com/auth/drive.file',
          'https://www.googleapis.com/auth/drive']
-service_file = r'C:\Users\LEGUser\Desktop\Projects\cryptotracker-327411-b7e2a6da147f.json'
+service_file = r'D:\Projects for SQL & Tableau\Python Project\cryptotracker-327411-b7e2a6da147f.json'
 gc = pygsheets.authorize(service_file=service_file)
 Worksheet = gc.open("Macquarie Doc Prep Pipeline")
 Status_Sheet = Worksheet.worksheet_by_title("Status")
 Bot_Review_Sheet = Worksheet.worksheet_by_title("Bot Review")
 
+#Email Extraction of the inbox for all unread files. Utilises string manipulation to extract the APP reference number and time library to extract the date and time. 
+#Marks message as read once this function has been run.
 def Email_Extraction():
     messages = inbox.Items
     messages.Sort("[ReceivedTime]", False)
     for msg in messages:
         if msg.Unread == True:
+            emptyList = []
             y = str(msg.Subject.encode('ascii', 'ignore')[-12:])
             p = str(y[-12:-1])
             date = msg.SentOn.strftime("%d/%m/%Y %H:%M:%S")
-            emptyList.append(p)
-            timeList.append(date)
+            print(p, date)
+            emptyList.insert(0, date)
+            emptyList.insert(1, p)
+            emptyList.insert(2, "2. Bots Running")
+            emptylist2.append(emptyList)
+            emptylist3.append(emptyList[0:2])
             msg.Unread = False
 
-    print(emptyList)
-    print(timeList)
-    print("Total Instructions to be processed: {}".format(len(emptyList)))
+    print("Total Instructions to be processed: {}".format(len(emptylist2)))
 
-
-# BELOW IS THE OLD CODE, IF SOMETHING GOES WRONG, USE THIS CODE AGAIN
-    # Macq_Inst = [message for message in inbox.Items if message.Unread == True]
-    # for msg in Macq_Inst:
-    #     y = str(msg.Subject.encode('ascii', 'ignore')[-12:])
-    #     p = str(y[-12:-1])
-    #     emptyList.append(p)
-    #     # msg.Unread = False
-    # # emptyList.reverse()
-    #
-    # print(emptyList)
-    #
-    # for item in messages:
-    #     if msg.Unread == True:
-    #         date = item.SentOn.strftime("%d/%m/%Y %H:%M:%S")
-    #         timeList.append(date)
-    # # timeList.reverse()
-    #
-    # print(timeList)
-    # print("Data exported to Google Sheets Successfully")
-
-#OLD CODE, USE THIS TO FILTER FOR AN EMPTY CELL IF CURRENT CODE DOESN'T WORK.
-    # str_list = list(filter(None, google_sheets.col_values(1)))
-    # str_list2 = len(google_sheets.get_all_values()) + 1
-
+# This function finds the first empty row in the Status spreadsheet and stores it.
 def findEmptyCell_status():
     list_of_lists = Status_Sheet.get_col(2)
 
     for emptycell in range(1,len(list_of_lists)):
         if len(list_of_lists[emptycell]) == 0:
-            print("The bot has started writing at Row {} on the Status Tab".format(emptycell+1))
             return emptycell
             break
 
+# This function checks the next empty cells to see if they are empty depending on how many apps needs to be processed.
+# If the rows are filled in, the program will exit to prevent overwriting data.
+def checkEmptyCell_Status():
+    list_of_lists = Status_Sheet.get_col(2)
+    emptycell = findEmptyCell_status()
+    total_files = len(emptylist2)
+    for i in range(emptycell, emptycell+total_files):
+        if len(list_of_lists[i]) != 0:
+            print("SYSTEM EXIT: There is a row already filled in at Row {}".format(i+1))
+            sys.exit()
+    print("The bot has started writing at Row {} on the Status Tab".format(emptycell+1))
 
-    # list_of_lists = Status_Sheet.get_all_values()
-    # for emptycell in range(1, len(list_of_lists)):
-    #
-    #     if len(list_of_lists[emptycell][1]) == 0:
-    #         print("The bot has started writing at Row {} on the Status Tab".format(emptycell+1))
-    #         return emptycell
-    #         break
 
+# This function finds the first empty row in the Bot Review spreadsheet and stores it.
 def findEmptyCell_Bot_Review():
-    list_of_lists = Bot_Review_Sheet.get_all_values()
+    list_of_lists = Bot_Review_Sheet.get_col(3)
     for emptycell in range(1, len(list_of_lists)):
 
-        if len(list_of_lists[emptycell][2]) == 0:
+        if len(list_of_lists[emptycell]) == 0:
             print("The bot has started writing at Row {} on the Bot_Review Tab".format(emptycell+1))
             return emptycell
             break
 
+# This function will now process the matrix and batch update the rows all at once on the status sheet.
 def writeEmptyCell_status():
     B_row = 1
-    C_row = 1
-    D_row = 1
     emptycell = findEmptyCell_status()
-    for dateandtime in timeList:
-        if Status_Sheet.get_value("B{}".format(emptycell+B_row)) == "":
-            x = str(emptycell + B_row)
-            Status_Sheet.update_value('B{}'.format(x), dateandtime)
-            B_row = B_row + 1
-        else:
-            print("There is a row already filled in at Row {}".format(emptycell + B_row))
-            sys.exit()
-
-    for APP in emptyList:
-        x = str(emptycell + C_row)
-        Status_Sheet.update_value('C{}'.format(x), APP)
-        C_row = C_row + 1
-        y = str(emptycell + D_row)
-        Status_Sheet.update_value('D{}'.format(y), "2. Bots Running")
-        D_row = D_row + 1
-
-    # for Status in emptyList:
-    #     x = str(emptycell + D_row)
-    #     Status_Sheet.update_value('D{}'.format(x), "2. Bots Running")
-    #     D_row = D_row + 1
-
+    x = str(emptycell + B_row)
+    Status_Sheet.update_values('B{}'.format(x), emptylist2)
+         
+# This function will now process the matrix and batch update the rows all at once on the bot_review sheet.
 def writeEmptyCell_Bot_Review():
 
     C_row = 1
-    D_row = 1
     emptycell = findEmptyCell_Bot_Review()
-
-    for dateandtime in timeList:
-        x = str(emptycell + C_row)
-        Bot_Review_Sheet.update_value('C{}'.format(x), dateandtime)
-        C_row = C_row + 1
-
-    for APP in emptyList:
-        x = str(emptycell + D_row)
-        Bot_Review_Sheet.update_value('D{}'.format(x), APP)
-        D_row = D_row + 1
+    x = str(emptycell + C_row)
+    Bot_Review_Sheet.update_values('C{}'.format(x), emptylist3)
 
 
 Email_Extraction()
+checkEmptyCell_Status()
 writeEmptyCell_status()
 writeEmptyCell_Bot_Review()
-
